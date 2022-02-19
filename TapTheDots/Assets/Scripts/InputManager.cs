f28806aeca660a8bf66b6c1038cbb6674a8a7e8d
JulieBodette <jr3python@gmail.com>
2022-02-18 19:08:52 -0500

    code detects tap on 3d object
    making this a commit so the code is saved
    
    will be deleted because this is a 2D game

--- TapTheDots/Assets/Scripts/InputManager.cs
+++ TapTheDots/Assets/Scripts/InputManager.cs
@@ -4,7 +4,28 @@ using UnityEngine.InputSystem; //tell it to use new input system
 
 public class InputManager : MonoBehaviour
 {
-    private TouchControls touchcontrols;
+    //for raycasting
+    private Camera mainCamera;
+
+
+
+   //uses raycasting
+    private void DetectObject()
+    {
+        //ScreenPointToRay shoots a ray from the camera to the position specified
+        Ray ray = mainCamera.ScreenPointToRay(touchcontrols.Touch.TouchPosition.ReadValue<Vector2>());
+        //this will only return the first thing it hits. You can also detect multiple things with RaycastAll
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider != null)
+            {
+                Debug.Log("hit" + hit.collider.tag);
+            }
+        }
+ 
+    }
+    private TouchControls touchcontrols; //TouchControls is the name of the auto-generated C# file
 
     private void Awake()
     {
@@ -23,6 +44,7 @@ public class InputManager : MonoBehaviour
 
     private void Start()
     {
+        mainCamera = Camera.main;//for raycasting
         //subscribe to event when we press 
         touchcontrols.Touch.TouchPress.started += ctx => StartTouch(ctx); //ctx is context
         //touchcontrols.Actionmap.Action    .started or .cancelled
@@ -31,7 +53,9 @@ public class InputManager : MonoBehaviour
 
     private void StartTouch(InputAction.CallbackContext context) {
         Debug.Log("touch started" + touchcontrols.Touch.TouchPosition.ReadValue<Vector2>());
-        }
+
+        DetectObject();//check if object was collided with
+    }
 
     private void EndTouch(InputAction.CallbackContext context)
     {
